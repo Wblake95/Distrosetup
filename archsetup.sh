d93b00d43ea64dd616a2a01f71a334ec769e13b8
Wesley Blake <wesleyblake95@gmail.com>
2022-07-21 15:55:46 -0700

    Add files via upload

--- archsetup.sh
+++ archsetup.sh
@@ -1,20 +1,22 @@
 #! /usr/bin/env bash
 
-echo "#########################################################################"
-echo "### Hello! This is my script that will set up my desktop.             ###"
-echo "### I don't have to do it anymore!                                    ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### Hello! This is my script that will set up my desktop.            ###"
+echo "### I don't have to do it anymore!                                   ###"
+echo "########################################################################"
 
-echo "#########################################################################"
-echo "### collecting keyrings                                               ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### collecting keyrings                                              ###"
+echo "########################################################################"
 sudo pacman -Sy --needed --noconfirm archlinux-keyring arcolinux-keyring
 
-echo "#########################################################################"
-echo "### updating system and installing packages                           ###"
-echo "### pacman flatpak pip                                                ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### updating system and installing packages                          ###"
+echo "### skell command for ArcoLinux                                      ###"
+echo "### pacman flatpak pip                                               ###"
+echo "########################################################################"
 sudo pacman -Syyu
+skell
 sudo pacman -S --needed --noconfirm vim emacs ranger vifm\
 	gimp krita discord steam flatpak python-pip
 
@@ -23,35 +25,34 @@ sudo flatpak install com.heroicgameslauncher.hgl
 
 pip update
 
-echo "#########################################################################"
-echo "### Installing ProtonGE                                               ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### Installing ProtonGE                                              ###"
+echo "########################################################################"
 pip install protonup
 protonup
 
-echo "#########################################################################"
-echo "### Setting up Steamdeck UI                                           ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### Setting up Steamdeck UI                                          ###"
+echo "########################################################################"
 cd ~/.steam/steam/package
 echo "steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d" > beta
 
-echo "#########################################################################"
-echo "### Running hblock and skell                                          ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### Running hblock and                                               ###"
+echo "########################################################################"
 hblock
-skell
 
-echo "#########################################################################"
-echo "### Setting up git and adding my repositories                         ###"
-echo "#########################################################################"
-bash gitsetup.sh
+echo "########################################################################"
+echo "### Setting up git and adding my repositories                        ###"
+echo "########################################################################"
+bash ~/Documents/distrosetup/gitsetup.sh
 cd ~/Documents
 git clone https://github.com/Wblake95/Linuxstuff
 git clone https://github.com/Wblake95/Python_Learning
 
-echo "#########################################################################"
-echo "### Setting pointers for config files to appropriate locations        ###"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### Setting pointers for config files to appropriate locations       ###"
+echo "########################################################################"
 ln -sbf --suffix=.bak ~/Documents/Linuxstuff/.bashrc ~/
 ln -sbf --suffix=.bak ~/Documents/Linuxstuff/.vimrc ~/
 ln -sbf --suffix=.bak ~/Documents/Linuxstuff/.zshrc ~/
@@ -60,6 +61,6 @@ ln -sbf --suffix=.bak ~/Documents/Linuxstuff/neofetch.config.conf\
        	~/.config/neofetch/config.conf
 ln -sbf --suffix=.bak ~/Documents/Linuxstuff/variety.conf ~/.config/variety/
 
-echo "#########################################################################"
-echo "### Done!                                                           #####"
-echo "#########################################################################"
+echo "########################################################################"
+echo "### Done!                                                            ###"
+echo "########################################################################"
--- gitsetup.sh
+++ gitsetup.sh
@@ -4,13 +4,12 @@ echo "########################################################################"
 echo "### Initiating git                                                   ###"
 echo "########################################################################"
 
-git init
-read "What is your name?" name
-git config --global user.name $name
-read "What is your email?" email
-git config --global user.email $email 
-read "What is your editor?" editor
-sudo git config --system core.editor $editor
+read name "What is your name?"
+git config --global user.name "$name"
+read email "What is your email?"
+git config --global user.email "$email" 
+read editor "What is your editor?"
+sudo git config --system core.editor "$editor"
 git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25000"
 git config --global push.default simple
